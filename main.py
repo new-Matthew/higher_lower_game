58a2fcd1ce223ad5985ac1ee4d661282ee498868
matheus <matheusrodrig16@gmail.com>
2025-01-27 10:19:07 -0300

    style: Logo added

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from random import randint
+from art import logo
+print(logo)
 intro = "Bem vindo ao jogo Maior ou Menor: Digite 1 se você acha que o primeiro número é maior e 2 se o segundo número é maior: "
+
 score = 0
 first_number = randint(1,100)
 second_number = randint(1,100)
-print(f"global {first_number}")
-print(f"global {second_number}")
 
 def greather_lower(f_number, s_number, score):
     end_game = False
     
-    type_answer = int(input("Digite 1 para escolher o primeiro, 2 para escolher o segundo!"))
+    type_answer = int(input("Digite 1 para escolher o primeiro, 2 para escolher o segundo! "))
 
     while not end_game:
         type_answer
