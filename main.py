d39ba1baa622af5f717694a44ba57b9ca71e8dea
matheus <matheusrodrig16@gmail.com>
2025-01-27 09:52:48 -0300

    feat: Base case choose a number and see if it is bigger or smaller and show score

---
+++ main.py
@@ -0,0 +1,50 @@
+from random import randint
+intro = "Bem vindo ao jogo Maior ou Menor: Digite 1 se você acha que o primeiro número é maior e 2 se o segundo número é maior: "
+score = 0
+first_number = randint(1,100)
+second_number = randint(1,100)
+print(f"global {first_number}")
+print(f"global {second_number}")
+
+def greather_lower(f_number, s_number, score):
+    end_game = False
+    
+    type_answer = int(input("Digite 1 para escolher o primeiro, 2 para escolher o segundo!"))
+
+    while not end_game:
+        type_answer
+        if type_answer == 1 and f_number > s_number:
+            print("parabéns você acertou")
+            score=+score+1
+            print(f"Sua pontuação é: {score}")
+            end_game = True
+
+        elif type_answer == 1 and f_number < s_number:
+            print("Você perdeu!!")
+            print(f"Sua pontuação é: {score}")
+            end_game = True
+
+        elif type_answer == 2 and f_number > s_number:
+            print("Você perdeu!!")
+            print(f"Sua pontuação é: {score}")
+            end_game = True
+
+        elif type_answer == 2 and f_number < s_number:
+            print("Parabéns você acertou!!")
+            score=+score+1
+            print(f"Sua pontuação é: {score}")
+            end_game = True
+
+        elif type_answer == 1 and f_number == s_number:
+            print("Você perdeu!!")
+            print(f"Sua pontuação é: {score}")
+            end_game = True
+            
+        elif type_answer == 2 and f_number == s_number:
+            print("Você perdeu!!")
+            print(f"Sua pontuação é: {score}")
+            end_game = True
+
+
+print(intro)
+greather_lower(first_number,second_number, score)
