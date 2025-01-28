13c81537ef7801ad8ac64e3fb4359cb051d8017b
matheus <matheusrodrig16@gmail.com>
2025-01-28 20:30:07 -0300

    feat: Implements logic of game higher lower

--- main.py
+++ main.py
@@ -5,33 +5,40 @@ from data import data_higher_lower
 print(logo)
 intro = "Bem vindo ao jogo Maior ou Menor: Digite 1 se você acha que o primeiro número é maior e 2 se o segundo número é maior: "
 print(intro)
-score = 0
 topic_1 = random.choice(data_higher_lower)
 topic_2 = random.choice(data_higher_lower)
 if topic_1 == topic_2:
     topic_2 = random.choice(data_higher_lower)
 
 def check_answer(guess_user, topic_value_1, topic_value_2):
-    topic_value_1 = topic_1["value"]
-    topic_value_2 = topic_2["value"]
-    if guess_user == 1:
-        topic_value_1 = topic_1["value"]
+    score = 0
 
-def show_berore(topic):
+    if topic_value_1 > topic_value_2 and guess_user == 1:
+        score =+ 1
+        print(f"Você acertou e seu score é: {score}")
+
+    elif topic_value_1 < topic_value_2 and guess_user == 1:
+        print(f"Você perdeu e seu score é: {score}")
+
+    elif topic_value_1 > topic_value_2 and guess_user == 2:
+        print(f"Você perdeu e seu score é: {score}")
+
+    elif topic_value_1 < topic_value_2 and guess_user == 2:
+        score =+ 1
+        print(f"Você acertou e seu score é: {score}")
+    else:
+        print("Insira um valor válido!")
+
+def show_before(topic):
 
     topic_name = topic["name"]
     return f"{topic_name} "
 
-def show_result(topic):
-    
-    topic_name = topic["name"]
-    topic_answer = topic["answer"]
-    return f"{topic_name} possui: {topic_answer}"
 
 print("Compare: ")
-print(f"opção 1: {show_berore(topic_1)}. ")
+print(f"opção 1: {show_before(topic_1)}. ")
 print(vs)
-print(f"opção 2: {show_berore(topic_2)}. ")
+print(f"opção 2: {show_before(topic_2)}. ")
 
 guess = int(input("Digite 1 ou 2: "))
 
@@ -40,23 +47,5 @@ topic_value_2 = topic_2["value"]
 
 check_answer(guess, topic_value_1, topic_value_2)
 
-print("Resposta: ")
-print(f"opção 1: {show_result(topic_1)}. ")
-print(vs)
-print(f"opção 2: {show_result(topic_2)}. ")
-
-
-"""
-def greather_lower(f_number, s_number, score):
-    end_game = False
-    type_answer = int(input("Digite 1 se você acha que o primeiro é maior ou 2 se acha que o segundo é maior! "))
-    if type_answer == 1:
-        topic_1
-    elif type_answer == 2:
-        topic_2
-
-    while not end_game:
-        type_answer
-"""
 
 
