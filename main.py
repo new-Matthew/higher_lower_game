c4252cde56a8c0b7cab6724800928c03818b34ca
matheus <matheusrodrig16@gmail.com>
2025-01-28 21:02:55 -0300

    feat: Add game loop with while for higher or lower logic

--- main.py
+++ main.py
@@ -6,10 +6,7 @@ print(logo)
 score = 0
 intro = "Bem vindo ao jogo Maior ou Menor: Digite 1 se você acha que o primeiro número é maior e 2 se o segundo número é maior: "
 print(intro)
-topic_1 = random.choice(data_higher_lower)
-topic_2 = random.choice(data_higher_lower)
-if topic_1 == topic_2:
-    topic_2 = random.choice(data_higher_lower)
+
 
 def check_answer(guess_user, topic_value_1, topic_value_2):
     if topic_value_1 > topic_value_2:
@@ -18,26 +15,37 @@ def check_answer(guess_user, topic_value_1, topic_value_2):
         return guess_user == 2
 
 def show_before(topic):
-
     topic_name = topic["name"]
     return f"{topic_name} "
 
+game_should_continue = True
 
-print("Compare: ")
-print(f"opção 1: {show_before(topic_1)}. ")
-print(vs)
-print(f"opção 2: {show_before(topic_2)}. ")
+while game_should_continue:
 
-guess = int(input("Digite 1 ou 2: "))
-
-topic_value_1 = topic_1["value"]
-topic_value_2 = topic_2["value"]
-
-is_correct = check_answer(guess, topic_value_1, topic_value_2)
+    topic_1 = random.choice(data_higher_lower)
+    topic_2 = random.choice(data_higher_lower)
+    if topic_1 == topic_2:
+        topic_2 = random.choice(data_higher_lower)
 
 
-if is_correct:
-    score =+ 1
-    print(f"Você acertou e seu score é: {score}")
-else:
-    print(f"Você errou e seu score final é: {score}")
+ 
+    print("Compare: ")
+    print(f"opção 1: {show_before(topic_1)}. ")
+    print(vs)
+    print(f"opção 2: {show_before(topic_2)}. ")
+
+
+
+    guess = int(input("Digite 1 ou 2: "))
+    print("=================================++++++=================================")
+    topic_value_1 = topic_1["value"]
+    topic_value_2 = topic_2["value"]
+
+    is_correct = check_answer(guess, topic_value_1, topic_value_2)
+
+    if is_correct:
+        score += 1
+        print(f"Você acertou e seu score é: {score}")
+    else:
+        print(f"Você errou e seu score final é: {score}")
+        game_should_continue = False
